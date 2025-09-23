82ba86d73db36ad8d98a33a5b7c5dd934d8b3437
souphia <sophia.machulek@escola.pr.gov.br>
2025-09-23 09:07:22 -0300

    fghfhfhfghfghfghgfhfgh

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    categoria,
+    pergunta,
+    resposta,
+)
+criaCartao(
+    'Dead By Daylight',
+    'O que é tunelar?',
+    'É quando o killer foca em uma só pessoa em vez de perseguir os outros players.'
+)
+criaCartao(
+    'Pergunta 2:',
+    'O que são perks?',
+    'São habilidades, de sobrevivente e killer, que podem ajudar em certos atributos na partida. exemplo: Deja Vu - de sobrevivente - Mostra os 3 geradores que estão mais proximos um do outro, e da 6% de bonus de velocidade ao fazer os geradores marcados. exemplo: Lightborn - de killer - As auras dos sobreviventes que tentam te cegar são mostradas por 10 segundos. Da imunidade contra ser cegado.',
+)
