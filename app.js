6e80f90be870b5e1850df2157c534a1be1fdab87
souphia <sophia.machulek@escola.pr.gov.br>
2025-10-01 10:53:55 -0300

    gdggg

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 function criarCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-  
     cartao.className = 'cartao'
+
     cartao.innerHTML = `
 <div class="cartao_conteudo">
 <h3>${categoria}</h3>
@@ -21,6 +21,6 @@ function criarCartao(categoria, pergunta, resposta) {
   respostaEstaVisivel = !respostaEstaVisivel
   cartao.classList.toggle('ative', respostaEstaVisivel)
     }
-    
-    let respostaEstaVisivel = true
+    cartao.addEventListener('click', viraCartao)
+
 }
