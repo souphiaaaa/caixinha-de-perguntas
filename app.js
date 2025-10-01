521e14481223500e8dc5686333abc2cb59ea06df
souphia <sophia.machulek@escola.pr.gov.br>
2025-10-01 10:39:47 -0300

    eefe

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 function criarCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
-    let respostaEstaVisivel = false
-    function viraCartao(){
-  respostaEstaVisivel = !respostaEstaVisivel
-
-    }
-    
-    let respostaEstaVisivel = true
+  
     cartao.className = 'cartao'
     cartao.innerHTML = `
 <div class="cartao_conteudo">
-<h3>Dead by Daylight</h3>
+<h3>${categoria}</h3>
 <div class="cartao_conteudo_pergunta">
-<p>O que é tunelar?</p>
+<p>${pergunta}/p>
 </div>
 <div class="cartao_conteudo_resposta">
-<p>É quando o killer foca em uma só pessoa em vez de perseguir os outros players.</p>
+<p>${resposta}</p>
 </div>
 </div>
 `
-
-
-
   container.appendChild(cartao)
+
+    let respostaEstaVisivel = false
+    function viraCartao(){
+  respostaEstaVisivel = !respostaEstaVisivel
+  cartao.classList.toggle('ative', respostaEstaVisivel)
+    }
+    
+    let respostaEstaVisivel = true
 }
